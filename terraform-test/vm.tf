35f432a9ea53f10501f5bd22cb97da93d05e5a84
Sujata Joshi <joshi.sujata.v@gmail.com>
2023-06-23 18:36:54 +0100

    Added change-4

--- terraform-test/vm.tf
+++ terraform-test/vm.tf
@@ -98,18 +98,19 @@ resource "null_resource" "ansible-config" {
     private_key = file("~/.ssh/id_rsa")
   }
 
-  provisioner "remote-exec" {
-    inline = [ "mkdir ~/.ssh" ]
-  }
-  
   provisioner "file" {
     source    = "~/.ssh/authorized_keys"
     destination = "~/.ssh/authorized_keys"
   } 
   
   provisioner "file" {
-    source = "~/.ssh/"
-    destination = "~/.ssh"
+    source = "~/.ssh/id_rsa.pub"
+    destination = "~/.ssh/id_rsa.pub"
+  }
+
+   provisioner "file" {
+    source = "~/.ssh/id_rsa"
+    destination = "~/.ssh/id_rsa"
   }
 }
 
